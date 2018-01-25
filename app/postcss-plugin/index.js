b286290fb16b338daddca0a2b67e27d7f7b4d849
Daniel Mears <clockworkdaniel@gmail.com>
2018-01-25 16:51:47 +0000

    changed postcss-plugin to work with double quotes in for image src too

--- app/postcss-plugin/index.js
+++ app/postcss-plugin/index.js
@@ -8,60 +8,60 @@ var sizeOf = require('image-size');
 
 module.exports = postcss.plugin('padding', function padding(options) {
 
-  var calculate = function(thisInclude, rule, dimensions) {
+	var calculate = function(thisInclude, rule, dimensions) {
 
-    let paddingBottom;
-    let vwHeight;
-    let maxHeight;
-    let finalParams;
+		let paddingBottom;
+		let vwHeight;
+		let maxHeight;
+		let finalParams;
 
-    if (thisInclude === "mobile") {
-      paddingBottom = dimensions.height/620 * 100;
-      finalParams = ", " + paddingBottom.toFixed(3) + "% )";
-    } else if (thisInclude === "image" || thisInclude === "extend-fw") {
-      paddingBottom = dimensions.height / 4000 * 100;
-      finalParams = ", " + paddingBottom.toFixed(3) + "% )";
-    } else if (thisInclude === "true-fw") {
-      vwHeight = dimensions.height / 4000 * 100;
-      vwHeight = ", " + vwHeight.toFixed(3) + "vw";
-      maxHeight = ", " + dimensions.height/2+"px )";
-      finalParams = vwHeight + maxHeight;
-    }
+		if (thisInclude === "mobile") {
+			paddingBottom = dimensions.height/620 * 100;
+			finalParams = ", " + paddingBottom.toFixed(3) + "% )";
+		} else if (thisInclude === "image" || thisInclude === "extend-fw") {
+			paddingBottom = dimensions.height / 4000 * 100;
+			finalParams = ", " + paddingBottom.toFixed(3) + "% )";
+		} else if (thisInclude === "true-fw") {
+			vwHeight = dimensions.height / 4000 * 100;
+			vwHeight = ", " + vwHeight.toFixed(3) + "vw";
+			maxHeight = ", " + dimensions.height/2+"px )";
+			finalParams = vwHeight + maxHeight;
+		}
 
-    newRule = "@include " + rule.params.substring(0, rule.params.length - 1) + finalParams;
+		newRule = "@include " + rule.params.substring(0, rule.params.length - 1) + finalParams;
 
-    return newRule;
+		return newRule;
 
-  }
+	};
 
 	var transform = function(css) {
 
-    var includes = [];
+		var includes = [];
 
-    //walk through every css rule
-    css.walkAtRules("include", rule => {
-      let thisInclude = rule.params.split("(")[0];
-      //...and that the include in question requires padding bottom
-      if (thisInclude === "extend-fw" || thisInclude === "image" || thisInclude === "true-fw" || thisInclude === "mobile") {
-        let params = rule.params;
-        //get the image URL from the include parameters
-        let imageUrl = params.split("'")[1];
-        //add preset to the image URL so that its height is correct for the calculation
-        let urlWithPreset = imageUrl + "?$retina$";
-        //parse the url ready for http.get
-        let options = url.parse(urlWithPreset);
+		//walk through every css rule
+		css.walkAtRules("include", rule => {
+			let thisInclude = rule.params.split("(")[0];
+			//...and that the include in question requires padding bottom
+			if (thisInclude === "extend-fw" || thisInclude === "image" || thisInclude === "true-fw" || thisInclude === "mobile") {
+				let params = rule.params;
+				//get the image URL from the include parameters
+				let imageUrl = params.indexOf("'") > -1 ? params.split("'")[1] : params.split('"')[1];
+				//add preset to the image URL so that its height is correct for the calculation
+				let urlWithPreset = imageUrl + "?$retina$";
+				//parse the url ready for http.get
+				let options = url.parse(urlWithPreset);
 
-        var includePromise = transformInclude(thisInclude, rule, options);
+				var includePromise = transformInclude(thisInclude, rule, options);
 
-        includes.push(includePromise);
-      }
-    });
+				includes.push(includePromise);
+			}
+		});
 
-    var includesPromise = Promise.all(includes);
+		var includesPromise = Promise.all(includes);
 
 		return includesPromise;
 		
-  };
+	};
 
 	var transformInclude = function(thisInclude, rule, options) {
 
@@ -69,13 +69,13 @@ module.exports = postcss.plugin('padding', function padding(options) {
 
 			https.get(options, function (response) {
 				let chunks = [];
-					response.on('data', function (chunk) {
+				response.on('data', function (chunk) {
 					chunks.push(chunk);
 				}).on('end', function() {
 					let buffer = Buffer.concat(chunks);
-          let dimensions = sizeOf(buffer);
+					let dimensions = sizeOf(buffer);
 
-          let newRule = calculate(thisInclude, rule, dimensions);			
+					let newRule = calculate(thisInclude, rule, dimensions);			
 
 					resolve(newRule);
 
@@ -84,11 +84,11 @@ module.exports = postcss.plugin('padding', function padding(options) {
 		});
 
 		var transformPromise = httpPromise.then(function(newRule) {
-        rule.replaceWith(newRule);
-    });
+			rule.replaceWith(newRule);
+		});
 
 		return transformPromise;
-  }
+	};
   
 	return transform;
 
